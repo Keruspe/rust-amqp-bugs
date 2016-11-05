0948d19cd8ee855641caaf56fbc19c7db6bc445d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-11-05 20:18:23 +0100

    add get-then-thread
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/bin/get-then-thread.rs
@@ -0,0 +1,31 @@
+extern crate amqp;
+extern crate std_semaphore;
+
+use std::{env,thread};
+use std::sync::Arc;
+use std::time::Duration;
+
+use amqp::{Basic,Session};
+use std_semaphore::Semaphore;
+
+fn main() {
+    let mut session = Session::open_url(env::var("AMQP_URL").unwrap().as_str()).unwrap();
+    let mut channel = session.open_channel(1).unwrap();
+    let semaphore   = Semaphore::new(5);
+    let sem_shared  = Arc::new(semaphore);
+
+    loop {
+        for get_result in channel.basic_get("keruspe.bug.test", false) {
+            let _sem = sem_shared.clone();
+            _sem.acquire();
+            thread::spawn(move || {
+                println!("New message: {}", String::from_utf8_lossy(&get_result.body));
+                get_result.ack();
+                _sem.release();
+            });
+        }
+
+        println!("No more messages, waiting for 60 seconds");
+        thread::sleep(Duration::from_secs(60));
+    }
+}
