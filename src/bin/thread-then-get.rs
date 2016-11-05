4ae25ad0a2ef1a48556957629eb9c01d1434f46a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-11-05 20:22:36 +0100

    add thread-then-get
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/bin/thread-then-get.rs
@@ -0,0 +1,35 @@
+extern crate amqp;
+extern crate std_semaphore;
+
+use std::{env,thread};
+use std::sync::Arc;
+use std::time::Duration;
+
+use amqp::{Basic,Session};
+use std_semaphore::Semaphore;
+
+fn main() {
+    let mut session = Session::open_url(env::var("AMQP_URL").unwrap().as_str()).unwrap();
+    let mut chan    = 1;
+    let semaphore   = Semaphore::new(5);
+    let sem_shared  = Arc::new(semaphore);
+
+    loop {
+        let _sem = sem_shared.clone();
+        _sem.acquire();
+
+        let mut channel = session.open_channel(chan).unwrap();
+        chan += 1;
+
+        thread::spawn(move || {
+            for get_result in channel.basic_get("keruspe.bug.test", false) {
+                println!("New message: {}", String::from_utf8_lossy(&get_result.body));
+                get_result.ack();
+            }
+
+            println!("No more messages, waiting for 60 seconds");
+            thread::sleep(Duration::from_secs(60));
+            _sem.release();
+        });
+    }
+}
